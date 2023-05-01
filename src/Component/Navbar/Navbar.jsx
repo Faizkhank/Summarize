3bd5fcfb42cb8ca5a3647864c8dad29a2058a745
faiz khan <fine7production@gmail.com>
2023-05-01 10:59:33 +0530

    commit

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
             Sum<span class="text-indigo-500 ">marize</span>
           </h1>
         </div>
-        <div className="p-6 mr-10">
+        <div className="p-5 mr-10">
           <a
             href="https://github.com/Faizkhank"
             className="bg-black text-white px-4 py-2 rounded-3xl cursor-pointer relative z-50 "
